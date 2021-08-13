d7e988e646267c6450f0e90584a68e21b093b7bb
Dmitri Gorun <iii448@mail.ru>
2021-08-13 23:00:21 +0500

    lesson12-localStorage

---
+++ script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+let todoData = [];
+
+todoData = JSON.parse(localStorage.getItem('todoData'));
+
+const render = function () {
+  todoList.textContent = '';
+  todoCompleted.textContent = '';
+
+  todoData.forEach(function (item, index) {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+
+    li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+      '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+      '</div>';
+
+    if (item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+
+    const btnTodoComplete = li.querySelector('.todo-complete');
+    btnTodoComplete.addEventListener('click', function () {
+      item.completed = !item.completed;
+      localStorage.setItem('todoData', JSON.stringify(todoData));
+      render();
+    });
+
+    const btnTodoRemove = li.querySelector('.todo-remove');
+    btnTodoRemove.addEventListener('click', function () {
+      todoData.splice(index, 1);
+      localStorage.setItem('todoData', JSON.stringify(todoData));
+      render();
+    });
+
+  });
+};
+
+todoControl.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (headerInput.value.trim() === '') {
+    alert('куку');
+  } else {
+    const newTodo = {
+      value: headerInput.value,
+      completed: false,
+    };
+    todoData.push(newTodo);
+    render();
+    headerInput.value = '';
+    localStorage.setItem('todoData', JSON.stringify(todoData));
+  }
+});
+
+headerInput.value = '';
+
+render();
